6edcd89d8059a49026dc39575c73475639dca551
Alice <Alice1820@github.com>
2017-11-06 21:05:27 +0800

    inceptionresnet4

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import torch
 from inceptionv4.pytorch_load import inceptionv4
-net = inceptionv4()
+from inceptionresnetv2.pytorch_load import inceptionresnetv2
+
+# net = inceptionv4()
+net = inceptionresnetv2()
 input = torch.autograd.Variable(torch.ones(1,3,299,299))
 output = net.forward(input)
 print (output)
